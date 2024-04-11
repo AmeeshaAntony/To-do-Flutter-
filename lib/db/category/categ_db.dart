c7dc96b448d6ae2d78a4ffcb895c193a1d363109
AmeeshaAntony <ameesha468@gmail.com>
2024-04-11 22:45:32 +0530

    Fifth Commit

---
+++ lib/db/category/categ_db.dart
@@ -0,0 +1,16 @@
+import 'package:money/models/category/category.dart';
+
+abstract class CategoryDBf{
+  //List<CategoryM> getcat();
+  Future<void> insertCategory(CategoryM value);//function to insert values.Declared as future bcz time delay occurs in insertion of data
+
+}
+
+class CategoryDB implements CategoryDBf{
+  @override
+  Future<void> insertCategory(CategoryM value) async {//what must occur on insertion must be written here.
+    
+  }
+
+}
+
--- lib/models/category/category.dart
+++ lib/models/category/category.dart
@@ -1,12 +1,21 @@
 
+import 'package:hive_flutter/hive_flutter.dart';
+
 enum CategoryType{
   inc,
   exp,
 }
+
+@HiveType(typeId: 1)
 class CategoryM{
+  @HiveField(0)
+  final String id;
+  @HiveField(1)
   final String name;
+  @HiveField(2)
   final bool isdeleted;
+  @HiveField(3)
   final CategoryType type;
 
-  CategoryM({required this.name, required this.type,this.isdeleted=false});
+  CategoryM({required this.id,required this.name, required this.type,this.isdeleted=false});
 }
